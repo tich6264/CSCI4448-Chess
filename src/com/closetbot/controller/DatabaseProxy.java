67c75d52da56addef636e61ee9a6d9ca46172834
Joseph Marylander <Joseph.Marylander@colorado.edu>
2016-11-02 18:22:42 -0600

    Added stubs to various classes

--- src/com/closetbot/controller/DatabaseProxy.java
+++ src/com/closetbot/controller/DatabaseProxy.java
@@ -1,7 +1,15 @@
 package com.closetbot.controller;
 
+import com.closetbot.model.User;
+
 /**
  * Created by Owner on 11/2/2016.
  */
 public class DatabaseProxy {
+    public User createUser(String username, String password){
+       return null;
+    }
+    public boolean saveUser(User u){
+        return true;
+    }
 }
--- src/com/closetbot/model/OutfitCloset.java
+++ src/com/closetbot/model/OutfitCloset.java
@@ -4,4 +4,15 @@ package com.closetbot.model;
  * Created by Owner on 11/2/2016.
  */
 public class OutfitCloset {
+    private Outfit[] outfits;
+    private User owner;
+    public void addOutfit(Outfit o){
+
+    }
+    public void removeOutfit(Outfit o){
+
+    }
+    public OutfitIterator iterator(){
+        return new OutfitIterator(outfits);
+    }
 }
