8b8c76f2f2b390549ba72f1e162cdda2c0beca14
Joseph Marylander <Joseph.Marylander@colorado.edu>
2016-11-18 16:27:58 -0700

    Added hibernate stuff for sqlite

--- src/com/closetbot/controller/DatabaseProxy.java
+++ src/com/closetbot/controller/DatabaseProxy.java
@@ -6,8 +6,9 @@ import com.closetbot.model.User;
  * Created by Owner on 11/2/2016.
  */
 public class DatabaseProxy {
+
     public User createUser(String username, String password){
-       return null;
+        return null;
     }
     public boolean saveUser(User u){
         return true;
