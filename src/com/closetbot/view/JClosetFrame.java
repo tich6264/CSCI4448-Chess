54dae82eec10bd4633760f28f2fb4fb3ca4e502b
Caleb Hsu <chialo.hsu@colorado.edu>
2016-11-18 14:43:48 -0700

    Adds panels to JFrame menu -- Caleb, Joey

--- src/com/closetbot/view/JClosetFrame.java
+++ src/com/closetbot/view/JClosetFrame.java
@@ -7,14 +7,15 @@ import java.awt.event.ActionListener;
 
 public class JClosetFrame extends JFrame {
 
-    private JPanel panel1 = new JPanel();
-    private JPanel panel2 = new JPanel();
+    private JPanel jAddOutfitPanel = new JAddOutfitPanel();
+    private JPanel jEditOutfitPanel = new JEditOutfitPanel();
+    private JPanel jGenerateOutfitPanel = new JGenerateOutfitPanel();
+    private JPanel jViewClosetPanel = new JViewClosetPanel();
+    private JPanel jViewOutfitClosetPanel = new JViewOutfitClosetPanel();
 
     public JClosetFrame() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         initMenu();
-        panel1.setBackground(Color.BLUE);
-        panel2.setBackground(Color.RED);
         setLayout(new BorderLayout());
     }
 
@@ -28,14 +29,25 @@ public class JClosetFrame extends JFrame {
     private void initMenu() {
         JMenuBar  menubar   = new JMenuBar();
         JMenu     menu      = new JMenu("Menu");
-        JMenuItem menuItem1 = new JMenuItem("Panel1");
-        JMenuItem menuItem2 = new JMenuItem("Panel2");
+        JMenuItem menuItem1 = new JMenuItem("Add Outfit");
+        JMenuItem menuItem2 = new JMenuItem("Edit Outfit");
+        JMenuItem menuItem3 = new JMenuItem("Generate Outfit");
+        JMenuItem menuItem4 = new JMenuItem("View Closet");
+        JMenuItem menuItem5 = new JMenuItem("View Outfit Closet");
+
         menubar.add(menu);
         menu.add(menuItem1);
         menu.add(menuItem2);
+        menu.add(menuItem3);
+        menu.add(menuItem4);
+        menu.add(menuItem5);
+
         setJMenuBar(menubar);
-        menuItem1.addActionListener(new MenuAction(panel1));
-        menuItem2.addActionListener(new MenuAction(panel2));
+        menuItem1.addActionListener(new MenuAction(jAddOutfitPanel));
+        menuItem2.addActionListener(new MenuAction(jEditOutfitPanel));
+        menuItem3.addActionListener(new MenuAction(jGenerateOutfitPanel));
+        menuItem4.addActionListener(new MenuAction(jViewClosetPanel));
+        menuItem5.addActionListener(new MenuAction(jViewOutfitClosetPanel));
     }
 
     private void changePanel(JPanel panel) {
