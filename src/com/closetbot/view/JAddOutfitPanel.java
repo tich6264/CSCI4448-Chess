df149b1970837a22bdfcdf0fd3b9500ad87feccd
Caleb Hsu <chialo.hsu@colorado.edu>
2016-11-18 16:25:19 -0700

    Creates basic panel for Add Outfit

--- src/com/closetbot/view/JAddOutfitPanel.java
+++ src/com/closetbot/view/JAddOutfitPanel.java
@@ -1,9 +1,38 @@
 package com.closetbot.view;
 
+import com.closetbot.model.Color;
+import com.closetbot.model.Pattern;
+import com.closetbot.model.Season;
+import com.closetbot.model.Type;
+
 import javax.swing.*;
 
 /**
  * Created by Owner on 11/18/2016.
  */
 public class JAddOutfitPanel extends JPanel {
+    public JAddOutfitPanel()
+    {
+        super();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // creates dropdowns
+        String[] types = new String[] {"Accessories", "Bottom", "Top", "Shoes"};
+
+        JComboBox<String> typeList = new JComboBox<String>(types);
+
+        JComboBox<Color> colorList = new JComboBox(Color.values());
+        JComboBox<Pattern> patternList = new JComboBox(Pattern.values());
+        JComboBox<Season> seasonList = new JComboBox(Season.values());
+
+        // add dropdowns to frame
+        panel.add(typeList);
+        panel.add(colorList);
+        panel.add(patternList);
+        panel.add(seasonList);
+
+        add(panel);
+    }
 }
