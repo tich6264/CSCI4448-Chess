52db296b165cd834b8aef2dfb2798730d38f7350
Caleb Hsu <chialo.hsu@colorado.edu>
2016-11-30 13:24:52 -0700

    Adds labels to dropdowns, dynamic subtype dropdown -- Caleb Hsu

--- src/com/closetbot/view/JAddOutfitPanel.java
+++ src/com/closetbot/view/JAddOutfitPanel.java
@@ -1,16 +1,25 @@
 package com.closetbot.view;
 
+import com.closetbot.model.*;
 import com.closetbot.model.Color;
-import com.closetbot.model.Pattern;
-import com.closetbot.model.Season;
-import com.closetbot.model.Type;
 
+import javax.persistence.Access;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Owner on 11/18/2016.
  */
 public class JAddOutfitPanel extends JPanel {
+    private JLabel typeLabel;
+    private JLabel subTypeLabel;
+    private JLabel colorLabel;
+    private JLabel patternLabel;
+    private JLabel seasonLabel;
+    private JButton saveButton;
+
     public JAddOutfitPanel()
     {
         super();
@@ -22,16 +31,68 @@ public class JAddOutfitPanel extends JPanel {
         String[] types = new String[] {"Accessories", "Bottom", "Top", "Shoes"};
 
         JComboBox<String> typeList = new JComboBox<String>(types);
-
         JComboBox<Color> colorList = new JComboBox(Color.values());
         JComboBox<Pattern> patternList = new JComboBox(Pattern.values());
         JComboBox<Season> seasonList = new JComboBox(Season.values());
 
-        // add dropdowns to frame
+        // this is empty until a type is selected
+        JComboBox subTypeList = new JComboBox();
+
+        // updates subtype dropdown based on type selection
+        typeList.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed( ActionEvent event ){
+                JComboBox typeList = (JComboBox) event.getSource();
+                Object selected = typeList.getSelectedItem();
+                if( selected.toString().equals("Accessories") ){
+                    subTypeList.setModel( new DefaultComboBoxModel<AccessoryTypes>(AccessoryTypes.values()));
+                    //JComboBox<AccessoryTypes> subTypeList = new JComboBox(AccessoryTypes.values());
+                }
+                else if( selected.toString().equals("Bottom") ){
+                    subTypeList.setModel( new DefaultComboBoxModel<BottomTypes>(BottomTypes.values()));
+                    // JComboBox<BottomTypes> subTypeList = new JComboBox(BottomTypes.values());
+                }
+                else if( selected.toString().equals("Top") ){
+                    subTypeList.setModel( new DefaultComboBoxModel<TopTypes>(TopTypes.values()));
+                    // JComboBox<TopTypes> subTypeList = new JComboBox(TopTypes.values());
+                }
+                else if( selected.toString().equals("Shoes") ){
+                    subTypeList.setModel( new DefaultComboBoxModel<ShoeTypes>(ShoeTypes.values()));
+                    // JComboBox<ShoeTypes> subTypeList = new JComboBox(ShoeTypes.values());
+                }
+                else{
+                    JComboBox subTypeList = new JComboBox();
+                }
+            }
+        } );
+
+        // creates labels
+        typeLabel = new JLabel("Type");
+        subTypeLabel = new JLabel("Subtype");
+        colorLabel = new JLabel("Color");
+        patternLabel = new JLabel("Pattern");
+        seasonLabel = new JLabel("Season");
+
+        // save selections
+        saveButton = new JButton("Save");
+
+        // add dropdowns & labels to frame
+        panel.add(typeLabel);
         panel.add(typeList);
+
+        panel.add(subTypeLabel);
+        panel.add(subTypeList);
+
+        panel.add(colorLabel);
         panel.add(colorList);
+
+        panel.add(patternLabel);
         panel.add(patternList);
+
+        panel.add(seasonLabel);
         panel.add(seasonList);
+
+        panel.add(saveButton);
 
         add(panel);
     }
